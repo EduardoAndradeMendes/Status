8154b649bf9e3f2bc8497825b96e919d86a0d7b4
Jake <jakehcasto@aol.com>
2016-03-30 11:34:00 -0400

    Create Main.php

---
+++ src/ImagicalGamer/Status/Main.php
@@ -0,0 +1,18 @@
+<?php
+namespace ImagicalGamer\Status;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+class Main extends PluginBase implements Listener{
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    if($cmd->getName() == "status"){
+		$tps = $this->getServer()->getTicksPerSecond();
+		$online = count($this->getServer()->getOnlinePlayers());
+		$load = $this->getServer()->getTickUsage();
+		$full = $this->getServer()->getMaxPlayers();
+		$sender->sendMessage(Color::GREEN . "Status" . "\n Current TPS:" . Color::LIGHT_PURPLE .  $tps . Color::GREEN . "\n Current load:" . Color::AQUA . $load . Color::GREEN . "\nAmount of Online Players:" . Color::RED .  $online);
+}
